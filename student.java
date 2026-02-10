1282f8dc67e96c5b11cd5231911e1fa73a796283
Henry Miller <hmbomb1221@gmail.com>
2026-02-09 19:27:59 -0800

    did student class (done i think)

--- student.java
+++ student.java
@@ -1,4 +1,33 @@
-package MealPay;
 public class student {
-    
+    private String name;
+    private double balance;
+    private int id;
+    private static int idGen = 1;
+
+    public student(String nameIn){
+        name = nameIn;
+        balance = 0;
+        id = idGen;
+        idGen++;
+    }
+
+    public String toString(){
+        return "Student name: " + name + ", ID number: " + id + ", Balance: $" + balance;
+    }
+
+    public void addFunds(double fundsAdded){
+        balance += fundsAdded;
+    }
+
+    public void makePurchaseStudent(){
+        balance -= 7;
+    }
+
+    public int getID(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
