0db646b91ee2c03252b6ccbd8c11b1a3a6e77e27
Henry Miller <hmbomb1221@gmail.com>
2026-02-09 19:28:11 -0800

    did purchase class (also done i think)

--- purchase.java
+++ purchase.java
@@ -1,4 +1,23 @@
-package MealPay;
 public class purchase {
-    
+    private student purchaser;
+    private int month;
+    private int day;
+
+    public purchase(student studentIn, int monthIn, int dayIn){
+        purchaser = studentIn;
+        month = monthIn;
+        day = dayIn;
+    }
+
+    public int getMonth(){
+        return month;
+    }
+
+    public int getDay(){
+        return day;
+    }
+
+    public student getStudent(){
+        return purchaser;
+    }
 }
