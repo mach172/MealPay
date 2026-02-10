d2cf23c7c44e951e358b0fb32b8506e0ee1f6a46
Ellery Engstrom <eengstrom26@students.polytechnic.org>
2026-02-10 12:21:44 -0800

    started system file

---
+++ PayMeal.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class PayMeal {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to the PayMeal system. ");
+        while(true){
+            System.out.println("Please select an option:");
+            System.out.println("1: Add new student");
+            System.out.println("2: Add funds to existing student's balance");
+            System.out.println("3: Make a purchase");
+            System.out.println("4: Print all students");
+            System.out.println("5: Print students with a negative balance");
+        }
+    }
+}
