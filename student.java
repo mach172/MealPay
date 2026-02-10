bd904034b57841d7311d380204cf18efd18d9ec2
Ellery Engstrom <eengstrom26@students.polytechnic.org>
2026-02-10 12:11:55 -0800

    added student list and student list methods

--- student.java
+++ student.java
@@ -1,14 +1,18 @@
+import java.util.*;
+
 public class Student {
     private String name;
     private double balance;
     private int id;
     private static int idGen = 1;
+    private static ArrayList<Student> studentList = new ArrayList<Student>();
 
     public Student(String nameIn){
         name = nameIn;
         balance = 0;
         id = idGen;
         idGen++;
+        studentList.add(this);
     }
 
     public String toString(){
@@ -29,5 +33,23 @@ public class Student {
 
     public String getName(){
         return name;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public static void printStudentList(){
+        for(Student a : studentList){
+            System.out.println(a.toString());
+        }
+    }
+
+    public static void printNegativeStudents(){
+        for(Student a : studentList){
+            if(a.getBalance() < 0){
+                System.out.println(a.toString());
+            }
+        }
     }
 }
