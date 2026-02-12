d73011a2605b0b545b035a8109608ab0293330fc
Henry Miller <hmbomb1221@gmail.com>
2026-02-12 13:37:15 -0800

    started interface for system

--- PayMeal.java
+++ PayMeal.java
@@ -1,7 +1,26 @@
 import java.util.*;
-import java.io.*;
 
 public class PayMeal {
+    public static ArrayList<Student> studentList = new ArrayList<Student>();
+
+    public static void addStudent(String nameIn){
+        studentList.add(new Student(nameIn));
+    }
+
+    public static void printStudentList(){
+        for(Student a : studentList){
+            System.out.println(a.toString());
+        }
+    }
+
+    public static void printNegativeStudents(){
+        for(Student a : studentList){
+            if(a.getBalance() < 0){
+                System.out.println(a.toString());
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -13,6 +32,73 @@ public class PayMeal {
             System.out.println("3: Make a purchase");
             System.out.println("4: Print all students");
             System.out.println("5: Print students with a negative balance");
+            //TODO: purchase histories
+
+            int choice = input.nextInt();
+            input.nextLine();
+
+            if(choice == 1){
+                System.out.println("Adding a new student...\n");
+                System.out.println("Please enter student's name:");
+                String tempName = input.nextLine();
+                addStudent(tempName);
+                System.out.println("\nNew student created: ");
+                System.out.println(studentList.get(studentList.size()-1).toString() + "\n");
+            }
+            else if(choice == 2){
+                System.out.println("Adding funds to existing student's balance...\n");
+                System.out.println("Please enter student's ID number: ");
+                int idNumber = input.nextInt();
+                boolean isFound = false;
+                for(Student a : studentList){
+                    if(a.getID() == idNumber){
+                        System.out.println("\nStudent selected is:");
+                        System.out.println(a.toString() + "\n");
+                        isFound = true;
+                    }
+                }
+                if(!isFound){
+                    System.out.println("\nError: student not found");
+                }
+                System.out.println("Please enter amount to add to student's balance: ");
+                double amtAdded = input.nextDouble();
+                for(Student a : studentList){
+                    if(a.getID() == idNumber){
+                        a.addFunds(amtAdded);
+                    }
+                }
+                System.out.println("Added $" + amtAdded + " to student.");
+                System.out.println("\nNew student information:");
+                for(Student a : studentList){
+                    if(a.getID() == idNumber){
+                        System.out.println(a.toString());
+                    }
+                }
+                System.out.println();
+            }
+            else if(choice == 3){
+                System.out.println("Making a purchase: \n");
+                System.out.println("Please enter month (1-12):");
+                int monthTemp = input.nextInt();
+                System.out.println("Please enter day:");
+                int dayTemp = input.nextInt();
+                System.out.println("Please enter student ID number: ");
+                int idNumber = input.nextInt();
+                boolean isFound = false;
+                for(Student a : studentList){
+                    if(a.getID() == idNumber){
+                        isFound = true;
+                        a.makePurchaseStudent();
+                        a.addPurchase(a, monthTemp, dayTemp);
+                        System.out.println("\nPurchase made. New student information: ");
+                        System.out.println(a.toString());
+                        System.out.println();
+                    }
+                }
+                if(!isFound){
+                    System.out.println("Error: student not found\n");
+                }
+            }
         }
     }
 }
